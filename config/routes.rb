6a96f0cb0db01063b5e1e5dd49acaaaad90bd866
votomaixe <vtomaixe@gmail.com>
2020-04-18 11:03:48 +0900

    learning CRUD(WIP)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-
   resources :learnings
-
-
-  # get 'learnings/index'
-  # get 'learnings/show'
-  # get 'learnings/new'
-  # get 'learnings/create'
-  # get 'learnings/edit'
-  # get 'learnings/update'
-  # get 'learnings/destroy'
 end
