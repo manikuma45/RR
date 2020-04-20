79ac6e878901324c7acb36e7b16075cae46034cd
votomaixe <vtomaixe@gmail.com>
2020-04-20 16:55:26 +0900

    ユーザー機能のCRUDを実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+    # binding.irb
+    # @user = current_user
+    # redirect_to(user_url(@user.id))
+  end
+
+  # def edit
+  #   redirect_to edit_user_registration_path(:current_user.id)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'learnings#index'
   resources :learnings
+
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+}
+
+resources :users, only: [:show]
+
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  
+
 end
