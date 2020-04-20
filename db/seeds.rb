5d893660d55479379dd56a3ca32ecfdc633f8397
votomaixe <vtomaixe@gmail.com>
2020-04-20 10:00:55 +0900

    index画面にラベルの内容を表示させた

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  Label.create!(label_name: "sample#{i + 1}")
+end
