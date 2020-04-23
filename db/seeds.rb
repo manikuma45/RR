e70f1397a67afe963db687c560e54dda1aa3762f
votomaixe <vtomaixe@gmail.com>
2020-04-23 20:23:34 +0900

    ラベルを横並びにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |i|
-  Label.create!(label_name: "sample#{i + 1}")
+  Label.create!(label_name: "label#{i + 1}")
 end
 
 30.times do |i|
